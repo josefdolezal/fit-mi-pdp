f994a97f880ccea86eaeb2653396be3ba429b31a
Josef Dolezal <pepik.ml@gmail.com>
2018-03-10 19:31:59 +0100

    Create base structure for semester project

---
+++ semester-project/semester-project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  semester-project
+//
+//  Created by Josef Dolezal on 10/03/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
