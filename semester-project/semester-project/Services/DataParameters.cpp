bf26413af1ecfb76e9884688350025a6c12a0927
Josef Dolezal <pepik.ml@gmail.com>
2018-04-10 14:07:55 +0200

    Create DataPerameters model

---
+++ semester-project/semester-project/Services/DataParameters.cpp
@@ -0,0 +1,15 @@
+//
+//  DataParameters.cpp
+//  semester-project
+//
+//  Created by Josef Dolezal on 10/04/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#include "DataParameters.hpp"
+
+DataParameters::DataParameters(Location location, CoverageSolution currentSolution, uint32_t currentDepth, uint32_t blacksTaken):
+    location(location), currentSolution(currentSolution), currentDepth(currentDepth), blacksTaken(blacksTaken)
+{
+    
+}
---
+++ semester-project/semester-project/Services/DataParameters.hpp
@@ -0,0 +1,25 @@
+//
+//  DataParameters.hpp
+//  semester-project
+//
+//  Created by Josef Dolezal on 10/04/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+#ifndef DataParameters_hpp
+#define DataParameters_hpp
+
+#include "ChessBoard.hpp"
+#include "CoverageSolution.hpp"
+
+struct DataParameters {
+public:
+    Location location;
+    CoverageSolution currentSolution;
+    uint32_t currentDepth;
+    uint32_t blacksTaken;
+
+    DataParameters(Location, CoverageSolution, uint32_t, uint32_t);
+};
+
+#endif /* DataParameters_hpp */
